2fc78004cfc59dd0f46aa018f13c2be66a93f157
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-04 01:02:13 -0400

    Update 02-get-breakglass-user.ps1

--- scripts/02-get-breakglass-user.ps1
+++ scripts/02-get-breakglass-user.ps1
@@ -1,1 +1,8 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$UserPrincipalName
+)
 
+Import-Module Microsoft.Graph.Identity.SignIns
+$result = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/users/$UserPrincipalName?`$select=id,displayName,userPrincipalName"
+$result
