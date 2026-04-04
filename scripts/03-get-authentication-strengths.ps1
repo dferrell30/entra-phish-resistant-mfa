2826f9b01748aeb8c9f720547a3ce28ede6fe505
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-04 01:02:32 -0400

    Update 03-get-authentication-strengths.ps1

--- scripts/03-get-authentication-strengths.ps1
+++ scripts/03-get-authentication-strengths.ps1
@@ -1,1 +1,2 @@
-
+$result = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/identity/conditionalAccess/authenticationStrength/policies"
+$result.value | Select-Object id, displayName, policyType, requirementsSatisfied
