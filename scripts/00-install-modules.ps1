8467f8568a85f45e30bbfbbeb84cea89afafe622
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-04 01:01:44 -0400

    Update 00-install-modules.ps1

--- scripts/00-install-modules.ps1
+++ scripts/00-install-modules.ps1
@@ -1,1 +1,12 @@
+$modules = @(
+    "Microsoft.Graph.Authentication",
+    "Microsoft.Graph.Identity.SignIns"
+)
 
+foreach ($module in $modules) {
+    if (-not (Get-Module -ListAvailable -Name $module)) {
+        Install-Module $module -Scope CurrentUser -Repository PSGallery -Force
+    }
+}
+
+Write-Host "Required modules installed."
