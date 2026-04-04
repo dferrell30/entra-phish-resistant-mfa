dc0be0932daf3e7119516cbce772779f321e3f16
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-04 01:02:50 -0400

    Update 04-enable-fido2-template.ps1

--- scripts/04-enable-fido2-template.ps1
+++ scripts/04-enable-fido2-template.ps1
@@ -1,1 +1,24 @@
+param(
+    [switch]$WhatIf
+)
 
+$current = Invoke-MgGraphRequest -Method GET -Uri "https://graph.microsoft.com/v1.0/authenticationMethodsPolicy/authenticationMethodConfigurations/fido2"
+$current | ConvertTo-Json -Depth 10
+
+if ($WhatIf) {
+    Write-Host "WhatIf specified. No changes made."
+    return
+}
+
+# Example PATCH body - customize before use
+$body = @{
+    "@odata.type" = "#microsoft.graph.fido2AuthenticationMethodConfiguration"
+    state = "enabled"
+    isAttestationEnforced = $false
+} | ConvertTo-Json
+
+Invoke-MgGraphRequest `
+    -Method PATCH `
+    -Uri "https://graph.microsoft.com/v1.0/authenticationMethodsPolicy/authenticationMethodConfigurations/fido2" `
+    -Body $body `
+    -ContentType "application/json"
