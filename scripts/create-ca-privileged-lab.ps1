e2b128c9971aa396cff5ee9b293c442f6ce3db76
Derrick Ferrell <54606286+dferrell30@users.noreply.github.com>
2026-04-04 00:20:08 -0400

    Update create-ca-privileged-lab.ps1

--- scripts/create-ca-privileged-lab.ps1
+++ scripts/create-ca-privileged-lab.ps1
@@ -1,1 +1,31 @@
+param(
+    [Parameter(Mandatory)]
+    [string]$BreakGlassObjectId
+)
 
+# Global Administrator role template ID
+$globalAdminRoleTemplateId = "62e90394-69f5-4237-9190-012177145e10"
+
+$params = @{
+    displayName = "CA - Privileged - MFA (Lab)"
+    state       = "enabledForReportingButNotEnforced"
+    conditions  = @{
+        users = @{
+            includeRoles = @($globalAdminRoleTemplateId)
+            excludeUsers = @($BreakGlassObjectId)
+        }
+        applications = @{
+            includeApplications = @("All")
+        }
+        clientAppTypes = @(
+            "browser",
+            "mobileAppsAndDesktopClients"
+        )
+    }
+    grantControls = @{
+        operator = "OR"
+        builtInControls = @("mfa")
+    }
+}
+
+New-MgIdentityConditionalAccessPolicy -BodyParameter $params
